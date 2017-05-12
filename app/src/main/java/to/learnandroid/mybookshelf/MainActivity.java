58f94eae42cd2e0e66eb077b3c34253b2611bcfd
Matt <matt@optimizeur.life>
2017-05-11 23:24:11 -0400

    XML: Created recycleView. Created cardView with book title and image.
    Java:
    Impletemented RecycleView Adapter
    Created Loader class
    TODO: Finish MainActivity implemetation
    
    Network:
    Setup Google Books API

--- app/src/main/java/to/learnandroid/mybookshelf/MainActivity.java
+++ app/src/main/java/to/learnandroid/mybookshelf/MainActivity.java
@@ -1,17 +1,25 @@
 package to.learnandroid.mybookshelf;
 
+import android.Manifest;
+import android.net.Uri;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 
+import java.net.URI;
 import java.util.List;
 
 import to.learnandroid.mybookshelf.data.model.Book;
 import to.learnandroid.mybookshelf.data.model.BookAdapter;
+import to.learnandroid.mybookshelf.data.model.BookLoader;
 import to.learnandroid.mybookshelf.utils.QueryUtils;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Book>> {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private static final String API_KEY = "AIzaSyBSXPLpKWjtQsw7jCuiFKbcWcgnVqq3PkQ";
+    private static final String GOOGLE_BOOKS_QUERY_URL = "https://www.googleapis.com/books/v1/volumes";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +27,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         RecyclerView bookList = (RecyclerView) findViewById(R.id.book_list);
+    }
+
+    @Override
+    public Loader<List<Book>> onCreateLoader(int id, Bundle args) {
+        Uri baseUri = Uri.parse(GOOGLE_BOOKS_QUERY_URL);
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+        uriBuilder.appendQueryParameter("key", API_KEY);
+        return new BookLoader(this, uriBuilder.toString());
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Book>> loader, List<Book> data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Book>> loader) {
+
     }
 }
--- app/src/main/java/to/learnandroid/mybookshelf/data/model/BookLoader.java
+++ app/src/main/java/to/learnandroid/mybookshelf/data/model/BookLoader.java
@@ -1,7 +1,7 @@
 package to.learnandroid.mybookshelf.data.model;
 
-import android.content.AsyncTaskLoader;
 import android.content.Context;
+import android.support.v4.content.AsyncTaskLoader;
 
 import java.util.List;
 
